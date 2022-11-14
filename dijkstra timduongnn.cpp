eb71df84e04f36fb00c467236d3b59213c54d92f
huyendieu909 <hd911910@gmail.com>
2022-11-15 00:47:30 +0700

    tìm đường ngắn nhất bằng dijkstra

---
+++ dijkstra timduongnn.cpp
@@ -0,0 +1,47 @@
+/*dijkstra tìm đường đi ngắn nhất*/
+#define toiuu ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int,int>> v[101];
+unordered_map<int,int> kc;
+unordered_map<int,int> check;
+priority_queue<pair<int,int>> q;
+void add(int x,int y,int w){
+    v[x].pb({y,w});
+    v[y].pb({x,w});
+}
+int main(){
+    //toiuu;
+    system("chcp 65001");
+    cout << "Nhập số đỉnh và số cạnh: ";
+    int dinh,canh;
+    cin >> dinh >> canh;
+//tạo graph
+    for (int i = 1;i<=canh;i++){
+        int x,y,z;
+        cin >> x >> y >> z;
+        add(x,y,z);
+    }
+//chọn đỉnh xuất phát để tính đường đi ngắn nhất
+    cout << "Nhập đỉnh xuất phát: ";int xp; cin >> xp;
+//set khoảng cách ban đầu
+    for (int i=1;i<=dinh;i++) kc[i] = 999999999;
+    kc[xp] = 0;
+//dijkstra
+    q.push({0,xp});
+    while (!q.empty()){
+        int a = q.top().second;
+        q.pop();
+        if (check[a]==1) continue;
+        check[a] = 1;
+        for (auto c: v[a]){
+            int b = c.first;
+            int w = c.second;
+            if (kc[b] > kc[a] + w) kc[b] = kc[a]+w; q.push({-kc[b],b});
+        }
+    }
+//đưa ra khoảng cách ngắn nhất tới đỉnh đích
+    cout << "Nhập đỉnh đích: ";
+    int d; cin >> d;
+    cout << kc[d];
+}
